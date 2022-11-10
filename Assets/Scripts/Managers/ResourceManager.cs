084bafb9ba3df85b50e5a899c5023ce5d0dcd6e6
FelixVenne <Felix.nachtigaller@web.de>
2022-11-10 14:57:57 +0100

    Witch Senses completed + AudioManager and ResourceManager Setup

---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,32 @@
+//ResourceManager holding most of the resources/assets used during runtime to avoid cluttering of the other scripts
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    //Singleton
+    public static ResourceManager Instance { set; get; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    //Resources
+
+    [Header("Audio")]
+
+    [Header("Witch Senses")]
+    public AudioClip audio_witchSenses_on;
+    public AudioClip audio_witchSenses_off;
+}
